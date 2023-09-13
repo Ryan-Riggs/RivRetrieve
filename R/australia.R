64f8943516624f3e8917d23c2a5b4114b6fdd82e
Riggs <rriggs@tamu.edu>
2023-09-13 15:51:33 -0500

    added get time_series

--- R/australia.R
+++ R/australia.R
@@ -167,3 +167,133 @@ parameters <- function(pars) {
     return(get(pars))
   }
 }
+
+get_timeseries <- function(parameter_type,
+                           station_number,
+                           start_date,
+                           end_date,
+                           tz,
+                           return_fields,
+                           ts_name) {
+
+  # If no tz supplied, get from jurisdiction
+  if (is.null(tz)) {
+    # From BoM website:
+    # Which time zones are the data displayed in?
+    #   Time of day is presented in local standard time. Coordinated Universal Timezones (UTC) are:
+    #
+    #   Eastern States (QLD, NSW, ACT, VIC, TAS) - UTC +10:00
+    #   Central States (NT, SA) - UTC +09:30
+    #   Western Australia - UTC +08:00.
+
+    # Get the station list and custom attributes to determine correct time zone
+    station_list <- get_station_list(parameter_type,
+                                     station_number,
+                                     return_fields="custom_attributes")
+    if (nrow(station_list) == 0) {
+      stop(paste("Station number", station_number, "is invalid"))
+    }
+
+    jurisdiction <- stringr::str_split_fixed(station_list$DATA_OWNER_NAME, " -", n = 2)[1]
+
+    # Time zones are selected where there is no DST
+    if (jurisdiction %in% c("ACT", "ACTNSW", "NSW", "QLD", "TAS", "VIC")) {
+      tz <- "Australia/Queensland" # AEST
+    } else if (jurisdiction %in% c("SA", "NT")) {
+      tz <- "Australia/Darwin" # ACST
+    } else if (jurisdiction == "WA") {
+      tz <- "Australia/Perth" # AWST
+    } else {
+      message("Jurisdiction not found, returning datetimes in UTC")
+      tz <- "UTC"
+    }
+  } else {
+    # Check if tz is valid
+    if (!tz %in% OlsonNames()) {
+      stop("Invalid tz argument. Check it is in OlsonNames().")
+    }
+    station_list <- get_station_list(parameter_type, station_number)
+    if (nrow(station_list) == 0) {
+      stop(paste("Station number", station_number, "is invalid"))
+    }
+  }
+
+  # Check string date input is valid
+  if (is.character(start_date)) {
+    start_date <- lubridate::as_date(start_date, format = "%Y-%m-%d")
+  }
+  if (is.character(end_date)) {
+    end_date <- lubridate::as_date(end_date, format = "%Y-%m-%d")
+  }
+  if (anyNA(c(start_date, end_date))) {
+    stop("Dates must be formatted as %Y-%m-%d (e.g. 2000-01-01)")
+  }
+  # Ensure start is less than end
+  if (start_date > end_date) {
+    stop("start_date must be less than end_date")
+  }
+
+  # Coerce to datetime
+  if (lubridate::is.Date(start_date)) {
+    start_date <- lubridate::force_tz(lubridate::as_datetime(start_date), tz = tz)
+    end_date <- lubridate::force_tz(lubridate::as_datetime(end_date), tz = tz)
+  } else if (!lubridate::is.POSIXt(start_date)) {
+    stop("Provide dates as a character, date, or datetime object")
+  }
+
+  # Fix the offset because BoM expects 00:00 style
+  # despite 0000 conforming to ISO8601
+  start_date <- sub(
+    "(.*\\+)(\\d{2})(\\d{2})", "\\1\\2:\\3",
+    lubridate::format_ISO8601(start_date, usetz = TRUE)
+  )
+  end_date <- sub(
+    "(.*\\+)(\\d{2})(\\d{2})", "\\1\\2:\\3",
+    lubridate::format_ISO8601(end_date, usetz = TRUE)
+  )
+
+  # Only accept one station at a time for now
+  if (length(station_number) > 1) {
+    stop("Only a single station can be requested at a time")
+  }
+
+  # If return_fields are missing return Timestamp, Value and Quality Code
+  if (missing(return_fields)) {
+    return_fields <- c("Timestamp", "Value", "Quality Code")
+  }
+
+  timeseries_id <- get_timeseries_id(
+    parameter_type,
+    station_number,
+    ts_name
+  )
+
+  timeseries_values <- get_timeseries_values(
+    timeseries_id$ts_id[1],
+    start_date,
+    end_date,
+    return_fields
+  )
+
+  # Only process data if it exists
+  if (nrow(timeseries_values) > 0) {
+    if ("Timestamp" %in% colnames(timeseries_values)) {
+      # nolint start
+      suppressMessages({
+        timeseries_values$Timestamp <- lubridate::as_datetime(timeseries_values$Timestamp, tz = tz)
+      })
+      timeseries_values <- dplyr::mutate_at(timeseries_values,
+                                            dplyr::vars(-"Timestamp"),
+                                            utils::type.convert,
+                                            as.is = TRUE
+      )
+      # nolint end
+    } else {
+      timeseries_values <- dplyr::mutate_all(timeseries_values,
+                                             utils::type.convert,
+                                             as.is = TRUE
+      )
+    }
+  }
+  return(timeseries_values)
+}
