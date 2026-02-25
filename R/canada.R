4e856f996f1074f886328644e7817ed0be852763
Ryan-Riggs <ryanriggs7@gmail.com>
2026-02-25 11:07:35 -0600

    Updated Canadian pull to retrieve from an API.

--- R/canada.R
+++ R/canada.R
@@ -16,8 +16,7 @@
 #' @return data frame of discharge time-series
 #' @examples
 #' \dontrun{
-#' #For the first time, you must run:
-#' tidyhydat::download_hydat()
+#' #Note this no longer relies on tidyhydat but rather retrieves data from an API.
 #' df = canada("01AD003")
 #' plot(df$Date, df$Q, type='l')
 #' }
@@ -35,15 +34,13 @@ canada <- function(site,
   start_date <- .get_start_date(start_date)
   end_date <- .get_end_date(end_date)
   column_name <- .get_column_name(variable)
-  if (variable == "discharge") {
-    original_data <- hy_daily_flows(site)
-  } else if (variable == "stage") {
-    original_data <- hy_daily_levels(site)
-  }
+  original_data = try(canada_pull(site,variable,start_date,end_date))
+  if (is.error(original_data)) return(stop('This gauge does not have a record associated with it and/or the agency website is down.'))
   original_data <- as_tibble(original_data)
+  value_name=colnames(original_data)[grep('Value',colnames(original_data))]
   data <- original_data %>%
-    dplyr::select(all_of(c("Date", "Value"))) %>%
-    rename(!!column_name := "Value") %>%
+    dplyr::select(all_of(c("Date", value_name))) %>%
+    rename(!!column_name := value_name) %>%
     filter(.data$Date >= start_date & .data$Date <= end_date)
   out <- new_tibble(
     data,
@@ -51,5 +48,11 @@ canada <- function(site,
     class = "rr_tbl"
   )
   return(out)
-  }
+}
+
+canada_pull=function(site,variable,start_date,end_date){
+  param=ifelse(variable=='stage','level','flow')
+  url=paste0('https://wateroffice.ec.gc.ca/services/daily_data/csv/inline?stations[]=',site,'&parameters[]=',param,'&start_date=',start_date,'&end_date=',end_date)
+  data=content(GET(url))
+}
 
